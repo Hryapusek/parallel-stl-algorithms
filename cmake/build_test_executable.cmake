fcba73a450f536398fd2388c6671f5d096a16e94
Abraamyan Alexander <samvelcc25@outlook.com>
2024-06-22 11:37:03 +0300

    cmake configured

---
+++ cmake/build_test_executable.cmake
@@ -0,0 +1,27 @@
+set(test_target "s0m4b0dY_parallel_algorithms_binary_test")
+
+include(cmake/install_gtest.cmake)
+
+install_gtest()
+
+set(TEST_EXECUTABLE_SOURCES
+    src/main.cpp
+    ${SOURCE_FILES}
+)
+set(TEST_EXECUTABLE_INCLUDES_PUBLIC
+    include/public/
+)
+
+add_executable(${test_target} ${SOURCE_FILES})
+
+target_sources(${test_target}
+    PRIVATE ${TEST_EXECUTABLE_SOURCES}
+    )
+
+target_include_directories(${test_target}
+    PUBLIC ${TEST_EXECUTABLE_INCLUDES_PUBLIC}
+)
+
+target_link_libraries(${test_target} gtest gtest_main)
+
+target_compile_features(${test_target} PUBLIC cxx_std_20)
