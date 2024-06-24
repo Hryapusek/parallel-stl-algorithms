19fde02b688bcec04ea6e341c7a631ccd4b3a03b
Abraamyan Alexander <samvelcc25@outlook.com>
2024-06-24 13:29:54 +0300

    generateRanges fixed, fully implemented

--- include/private/s0_utils.hpp
+++ include/private/s0_utils.hpp
@@ -3,28 +3,39 @@
 
 #include <vector>
 
+template <class Iterator_t>
+std::vector<std::pair<Iterator_t, Iterator_t>> generateRanges(Iterator_t begin, Iterator_t end, int max_number_of_threads);
 
-template< class Iterator_t >
-auto generateRanges(Iterator_t begin, Iterator_t end);
-
-
-template< class Iterator_t >
-auto generateRanges(Iterator_t begin, Iterator_t end, int max_number_of_threads)
+template <class Iterator_t>
+std::vector<std::pair<Iterator_t, Iterator_t>> generateRanges(Iterator_t begin, Iterator_t end, int max_number_of_threads)
 {
-    auto n_cpu = max_number_of_threads;
-    auto jobs_on_kernel = length / n_cpu;
-    auto length = std::distance(begin, end);
+  auto length = std::distance(begin, end);
+  auto n_cpu = max_number_of_threads;
+  auto jobs_on_kernel = length / n_cpu;
+  auto minimum_required_jobs_for_parallel_execution = 20;
+  if (jobs_on_kernel < minimum_required_jobs_for_parallel_execution)
+  {
+    return {{begin, end}};
+  }
 
-    std::vector<std::pair<Iterator_t, Iterator_t>> ranges;
+  std::vector<std::pair<Iterator_t, Iterator_t>> ranges;
 
-    for (auto i = 0, it = begin; i < n_cpu; ++i)
+  for (auto i = 0, it = begin; i < n_cpu; ++i)
+  {
+    auto left = it;
+    auto right = it;
+    if (i == n_cpu - 1)
     {
-      auto left = it;
-      auto right = std::advance(it, jobs_on_kernel);
-      it = right;
-      ranges.push_back({left, right});
+      right = end;
     }
-    return ranges;
+    else
+    {
+      std::advance(it, jobs_on_kernel);
+      right = it;
+    }
+    ranges.push_back({left, right});
+  }
+  return ranges;
 }
 
 #endif
