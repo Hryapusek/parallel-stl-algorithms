116e8d6ebb976576044f89cb3a7eed5fcee82865
Abraamyan Alexander <samvelcc25@outlook.com>
2024-06-24 13:31:29 +0300

    tests from main removed

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
-#include "parallel_algorithms_openmp.hpp"
 #include <gtest/gtest.h>
 
 int main()
 {
     ::testing::InitGoogleTest();
     return RUN_ALL_TESTS();
-}
-
-TEST(Basic, usage)
-{
-    std::vector<int> vec{1, 2, 3};
-    s0m4b0dY::OpenMPI().reduce(vec.begin(), vec.end());
 }
