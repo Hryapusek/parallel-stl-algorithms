fdfbc23101e880bb595070b2f5e4384fce94e6d1
Abraamyan Alexander <samvelcc25@outlook.com>
2024-06-29 17:50:09 +0300

    files moved to the directories respectively

--- openmp-parallel-implementation/cmake/install_gtest.cmake
+++ openmp-parallel-implementation/cmake/install_gtest.cmake
@@ -2,7 +2,8 @@ function(install_gtest)
     include(FetchContent)
         FetchContent_Declare(
             googletest
-            GIT https://github.com/google/googletest.git
+            GIT_REPOSITORY https://github.com/google/googletest.git
+            GIT_TAG f8d7d77
         )
     # For Windows: Prevent overriding the parent project's compiler/linker settings
     set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
