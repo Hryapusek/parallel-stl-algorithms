16f1df08662e0c913a1fb9772f1bca303e3c486e
Abraamyan Alexander <samvelcc25@outlook.com>
2024-07-02 19:09:11 +0300

    openMP reduce with init value added

--- openmp-parallel-implementation/gtest/src/s0_omp_parallel_algorithms_test.cpp
+++ openmp-parallel-implementation/gtest/src/s0_omp_parallel_algorithms_test.cpp
@@ -6,11 +6,21 @@
 
 #include "s0_parallel_algorithms_openmp.hpp"
 
-TEST(reduceParallelAlgo, VectorOf500Elements)
+TEST(reduceParallelAlgorithm, VectorOf500ElementsDefaultReduce)
 {
     auto range = std::ranges::views::iota(0, 500);
     std::vector<int> arr(range.begin(), range.end());
     s0m4b0dY::OpenMPI openMPI;
     auto result = openMPI.reduce(arr.begin(), arr.end());
     ASSERT_EQ(result, std::reduce(arr.begin(), arr.end()));
+}
+
+TEST(reduceParallelAlgorithm, VectorOf500ElementsInitValuePassed)
+{
+    auto range = std::ranges::views::iota(0, 500);
+    std::vector<int> arr(range.begin(), range.end());
+    s0m4b0dY::OpenMPI openMPI;
+    int initValue = 200;
+    auto result = openMPI.reduce(arr.begin(), arr.end(), initValue);
+    ASSERT_EQ(result, initValue + std::reduce(arr.begin(), arr.end()));
 }
--- openmp-parallel-implementation/include/public/s0_parallel_algorithms_openmp.hpp
+++ openmp-parallel-implementation/include/public/s0_parallel_algorithms_openmp.hpp
@@ -12,33 +12,77 @@ namespace s0m4b0dY
 {
   class OpenMPI
   {
+    template < class T >
+    using IteratorValueType = _helpers::IteratorValueType<T>;
   public:
     template <_helpers::AddableIterator Iterator_t>
-    _helpers::IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end);
+    IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end);
+
+    template <_helpers::AddableIterator Iterator_t>
+    IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end, IteratorValueType<Iterator_t>::value_type initValue);
   };
 
   template <_helpers::AddableIterator Iterator_t>
   inline _helpers::IteratorValueType<Iterator_t>::value_type OpenMPI::reduce(Iterator_t begin, Iterator_t end)
   {
     using value_type = _helpers::IteratorValueType<Iterator_t>::value_type;
-    const value_type init_value = 0;
+    const value_type initValue = 0;
     std::vector<std::pair<Iterator_t, Iterator_t>> ranges = generateRanges(begin, end, omp_get_max_threads());
-    std::vector<value_type> results(ranges.size(), init_value);
-    #pragma omp parallel for
-    for (auto i = 0; i < ranges.size(); ++i)
+    std::vector<value_type> results(ranges.size(), initValue);
+    try
     {
-      const auto &range = ranges[i];
-      value_type result = init_value;
-      for (auto it = range.first; it != range.second; it++)
+      #pragma omp parallel for
+      for (auto i = 0; i < ranges.size(); ++i)
       {
-        result += *it;
+        const auto &range = ranges[i];
+        value_type result = initValue;
+        for (auto it = range.first; it != range.second; it++)
+        {
+          result += *it;
+        }
+        results[i] = result;
       }
-      results[i] = result;
     }
-    auto result = init_value;
-    for (const auto &local_result : results)
+    catch(const std::exception &e)
     {
-      result += local_result;
+      throw;
+    }
+    auto result = initValue;
+    for (const auto &localResult : results)
+    {
+      result += localResult;
+    }
+    return result;
+  }
+
+  template<_helpers::AddableIterator Iterator_t>
+  inline _helpers::IteratorValueType<Iterator_t>::value_type OpenMPI::reduce(Iterator_t begin, Iterator_t end, IteratorValueType<Iterator_t>::value_type initValue)
+  {
+    using value_type = _helpers::IteratorValueType<Iterator_t>::value_type;
+    std::vector<std::pair<Iterator_t, Iterator_t>> ranges = generateRanges(begin, end, omp_get_max_threads());
+    std::vector<value_type> results(ranges.size(), initValue);
+    try
+    {
+      #pragma omp parallel for
+      for (auto i = 0; i < ranges.size(); ++i)
+      {
+        const auto &range = ranges[i];
+        value_type result = 0;
+        for (auto it = range.first; it != range.second; it++)
+        {
+          result += *it;
+        }
+        results[i] = result;
+      }
+    }
+    catch(const std::exception &e)
+    {
+      throw;
+    }
+    auto result = initValue;
+    for (const auto &localResult : results)
+    {
+      result += localResult;
     }
     return result;
   }
