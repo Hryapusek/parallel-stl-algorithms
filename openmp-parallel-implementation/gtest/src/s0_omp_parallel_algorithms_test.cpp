12bdd16ce5521607beb20713625801c105530cb4
Abraamyan Alexander <samvelcc25@outlook.com>
2024-07-04 16:19:43 +0300

    threading updated

--- openmp-parallel-implementation/gtest/src/s0_omp_parallel_algorithms_test.cpp
+++ openmp-parallel-implementation/gtest/src/s0_omp_parallel_algorithms_test.cpp
@@ -24,3 +24,25 @@ TEST(reduceParallelAlgorithm, VectorOf500ElementsInitValuePassed)
     auto result = openMPI.reduce(arr.begin(), arr.end(), initValue);
     ASSERT_EQ(result, initValue + std::reduce(arr.begin(), arr.end()));
 }
+
+TEST(findIfParallelAlgorithm, SearchWithLambda)
+{
+    auto range = std::ranges::views::iota(0, 500);
+    std::vector<int> arr(range.begin(), range.end());
+    s0m4b0dY::OpenMPI openMPI;
+    int initValue = 200;
+    constexpr int searchVaule = 420;
+    auto result = openMPI.find_if(arr.begin(), arr.end(), [](int value){return value == searchVaule;});
+    ASSERT_NE(result, arr.end());
+    ASSERT_EQ(*result, searchVaule);
+}
+
+TEST(findIfParallelAlgorithm, SearchWithLambdaNonExistValue)
+{
+    auto range = std::ranges::views::iota(0, 500);
+    std::vector<int> arr(range.begin(), range.end());
+    s0m4b0dY::OpenMPI openMPI;
+    int initValue = 200;
+    auto result = openMPI.find_if(arr.begin(), arr.end(), [](int value){return false;});
+    ASSERT_EQ(result, arr.end());
+}
--- openmp-parallel-implementation/include/public/s0_parallel_algorithms_openmp.hpp
+++ openmp-parallel-implementation/include/public/s0_parallel_algorithms_openmp.hpp
@@ -3,6 +3,8 @@
 
 #include <type_traits>
 #include <vector>
+#include <optional>
+#include <atomic>
 #include <omp.h>
 
 #include "CommonUtils/s0_type_traits.hpp"
@@ -20,39 +22,57 @@ namespace s0m4b0dY
 
     template <_helpers::AddableIterator Iterator_t>
     IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end, IteratorValueType<Iterator_t>::value_type initValue);
+
+    template <_helpers::AddableIterator Iterator_t, _helpers::Predicate<typename IteratorValueType<Iterator_t>::value_type> Predicate>
+    Iterator_t find_if(Iterator_t begin, Iterator_t end, Predicate &&unaryFunction);
   };
 
   template <_helpers::AddableIterator Iterator_t>
   inline _helpers::IteratorValueType<Iterator_t>::value_type OpenMPI::reduce(Iterator_t begin, Iterator_t end)
   {
     using value_type = _helpers::IteratorValueType<Iterator_t>::value_type;
-    const value_type initValue = 0;
     std::vector<std::pair<Iterator_t, Iterator_t>> ranges = generateRanges(begin, end, omp_get_max_threads());
-    std::vector<value_type> results(ranges.size(), initValue);
+    std::vector<std::optional<value_type>> results(ranges.size(), std::nullopt);
     try
     {
       #pragma omp parallel for
       for (auto i = 0; i < ranges.size(); ++i)
       {
         const auto &range = ranges[i];
-        value_type result = initValue;
-        for (auto it = range.first; it != range.second; it++)
+        auto it = range.first;
+        if (it != range.second)
         {
-          result += *it;
+          value_type result = *it++;
+          for (; it != range.second; it++)
+          {
+            result += *it;
+          }
+          results[i] = result;
         }
-        results[i] = result;
       }
     }
     catch(const std::exception &e)
     {
       throw;
     }
-    auto result = initValue;
-    for (const auto &localResult : results)
-    {
-      result += localResult;
+    std::optional<value_type> result;
+    for (std::optional<value_type> &localResult : results)
+    { 
+      if (localResult.has_value())
+      {
+        if (result.has_value())
+        {
+          *result += std::move(localResult).value();
+        }
+        else
+        {
+          result = std::move(localResult).value();
+        }
+      }
     }
-    return result;
+    if (not result.has_value())
+      throw std::logic_error("No values passed in reduce algorithm");
+    return *result;
   }
 
   template<_helpers::AddableIterator Iterator_t>
@@ -60,19 +80,23 @@ namespace s0m4b0dY
   {
     using value_type = _helpers::IteratorValueType<Iterator_t>::value_type;
     std::vector<std::pair<Iterator_t, Iterator_t>> ranges = generateRanges(begin, end, omp_get_max_threads());
-    std::vector<value_type> results(ranges.size(), initValue);
+    std::vector<std::optional<value_type>> results(ranges.size(), std::nullopt);
     try
     {
       #pragma omp parallel for
       for (auto i = 0; i < ranges.size(); ++i)
       {
         const auto &range = ranges[i];
-        value_type result = 0;
-        for (auto it = range.first; it != range.second; it++)
+        auto it = range.first;
+        if (it != range.second)
         {
-          result += *it;
+          value_type result = *it++;
+          for (; it != range.second; it++)
+          {
+            result += *it;
+          }
+          results[i] = result;
         }
-        results[i] = result;
       }
     }
     catch(const std::exception &e)
@@ -80,12 +104,53 @@ namespace s0m4b0dY
       throw;
     }
     auto result = initValue;
-    for (const auto &localResult : results)
-    {
-      result += localResult;
+    for (std::optional<value_type> &localResult : results)
+    { 
+      if (localResult.has_value())
+        result += std::move(localResult).value();
     }
     return result;
   }
+
+  template<_helpers::AddableIterator Iterator_t, _helpers::Predicate<typename _helpers::IteratorValueType<Iterator_t>::value_type> Predicate>
+  inline Iterator_t OpenMPI::find_if(Iterator_t begin, Iterator_t end, Predicate && unaryFunction)
+  {
+    using value_type = _helpers::IteratorValueType<Iterator_t>::value_type;
+    std::vector<std::pair<Iterator_t, Iterator_t>> ranges = generateRanges(begin, end, omp_get_max_threads());
+    std::vector<std::optional<Iterator_t>> results(ranges.size(), std::nullopt);
+    bool found = false;
+    try
+    {
+      #pragma omp parallel for
+      for (auto i = 0; i < ranges.size(); ++i)
+      {
+        const auto &range = ranges[i];
+        for (auto it = range.first; it != range.second; it++)
+        {
+          bool local_found = false;
+          #pragma omp atomic read
+            local_found = found;
+          if (local_found)
+            break;
+          if (unaryFunction(*it))
+          {
+            results[i] = it;
+          }
+        }
+      }
+    }
+    catch(const std::exception &e)
+    {
+      throw;
+    }
+    for (std::optional<Iterator_t> &localResult : results)
+    { 
+      if (localResult.has_value())
+        return std::move(localResult).value();
+    }
+    return end;
+  }
+
 } // namespace s0m4b0dY
 
 #endif
