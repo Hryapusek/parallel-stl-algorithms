49e3ef745ef696402382777cf4ed059bacdb85a1
Abraamyan Alexander <samvelcc25@outlook.com>
2024-07-11 01:34:48 +0300

    multimap replaced map and some logic was changed

--- utils-library/include/public/CommonUtils/s0_utils.hpp
+++ utils-library/include/public/CommonUtils/s0_utils.hpp
@@ -4,101 +4,130 @@
 #include <vector>
 
 template <class Iterator_t>
-std::vector<std::pair<Iterator_t, Iterator_t>> generateRanges(Iterator_t begin, Iterator_t end, int max_number_of_threads);
+std::vector<std::pair<Iterator_t, Iterator_t> > generateRanges(Iterator_t begin, Iterator_t end, int max_number_of_threads);
 
 template <class Iterator_t>
-std::vector<std::pair<Iterator_t, Iterator_t>> generateRanges(Iterator_t begin, Iterator_t end, int max_number_of_threads)
+std::vector<std::pair<Iterator_t, Iterator_t> > generateRanges(Iterator_t begin, Iterator_t end, int max_number_of_threads)
 {
-  auto length = std::distance(begin, end);
-  auto n_cpu = max_number_of_threads;
-  auto jobs_on_kernel = length / n_cpu;
-  auto minimum_required_jobs_for_parallel_execution = 20;
-  if (jobs_on_kernel < minimum_required_jobs_for_parallel_execution)
-  {
-    return {{begin, end}};
-  }
+	auto length = std::distance(begin, end);
+	auto n_cpu = max_number_of_threads;
+	auto jobs_on_kernel = length / n_cpu;
+	auto minimum_required_jobs_for_parallel_execution = 20;
+	if (jobs_on_kernel < minimum_required_jobs_for_parallel_execution)
+	{
+		return {{begin, end}};
+	}
 
-  std::vector<std::pair<Iterator_t, Iterator_t>> ranges;
+	std::vector<std::pair<Iterator_t, Iterator_t> > ranges;
 
-  for (auto i = 0, it = begin; i < n_cpu; ++i)
-  {
-    auto left = it;
-    auto right = it;
-    if (i == n_cpu - 1)
-    {
-      right = end;
-    }
-    else
-    {
-      std::advance(it, jobs_on_kernel);
-      right = it;
-    }
-    ranges.push_back({left, right});
-  }
-  return ranges;
+	for (auto i = 0, it = begin; i < n_cpu; ++i)
+	{
+		auto left = it;
+		auto right = it;
+		if (i == n_cpu - 1)
+		{
+			right = end;
+		}
+		else
+		{
+			std::advance(it, jobs_on_kernel);
+			right = it;
+		}
+		ranges.push_back({left, right});
+	}
+	return ranges;
 }
 
-  /// @return Comparator that returns true if swap was performed. 
-  /// @return False otherwise
-  template < class Comparator, class HashTable >
-  auto createInplaceComparator(Comparator &comparator, HashTable &hashTable)
-  {
-    return [&comparator, &hashTable](auto &lhs, auto &rhs) -> bool
-    {
-      if (comparator(*hashTable[lhs], *hashTable[rhs]))
-      {
-        return false;
-      }
-      else
-      {
-        std::swap(lhs, rhs);
-        return true;
-      }
-    };
-  }
+/// @return Comparator that returns true if swap was performed.
+/// @return False otherwise
+template < class Comparator, class HashTable >
+auto createInplaceComparator(Comparator &comparator, HashTable &hashTable)
+{
+	return [&comparator, &hashTable](auto &lhs, auto &rhs) -> bool
+		   {
+			   if (comparator(*hashTable.find(lhs)->second, *hashTable.find(rhs)->second))
+			   {
+				   return false;
+			   }
+			   else
+			   {
+				   std::swap(lhs, rhs);
+				   return true;
+			   }
+		   };
+}
 
-  template<std::forward_iterator InputIterator_t, class Hash_t, class Value_t, class HashFunction, class Comparator>
-  inline std::pair<std::vector<Hash_t>, std::unordered_map<Hash_t,Value_t *>> hash_sequence(InputIterator_t begin, InputIterator_t end, HashFunction hashFunction, Comparator comparator)
-  {
-    std::unordered_map<Hash_t, Value_t *> hashTable;
-    std::vector<Hash_t> hashedValues;
+template<std::forward_iterator InputIterator_t, class Hash_t, class Value_t, class HashFunction, class Comparator>
+inline std::pair<std::vector<Hash_t>, std::unordered_multimap<Hash_t, Value_t *> > hash_sequence(InputIterator_t begin, InputIterator_t end, HashFunction hashFunction, Comparator comparator)
+{
+	std::unordered_multimap<Hash_t, Value_t *> hashTable;
+	std::vector<Hash_t> hashedValues;
 
-    for (auto it = begin; it != end; it++)
+	for (auto it = begin; it != end; it++)
+	{
+		auto hash = hashFunction(*it);
+		hashTable.insert({hash, &*it});
+		hashedValues.push_back(hash);
+	}
+
+	return {hashedValues, hashTable};
+}
+
+template< std::forward_iterator InputIterator_t, class HashFunction, class Hash_t, class Value_t >
+void placeElementsInCorrectPositions(InputIterator_t begin, InputIterator_t end, HashFunction hashFunction, const std::vector<Hash_t> &hashValues, std::unordered_multimap<Hash_t, Value_t *> &hashTable)
+{
+    std::unordered_multimap<Hash_t, Value_t> popValues;
+
+    for (auto src_it = begin, correctHash = hashValues.begin(); src_it != end; ++src_it, ++correctHash)
     {
-      auto hash = hashFunction(*it);
-      hashTable.insert({hash, &*it});
-      hashedValues.push_back(hash);
+        auto hash = hashFunction(*src_it);
+
+        // Skip elements already in the correct position
+        if (hash == *correctHash)
+        {
+            continue;
+        }
+
+        // Move the element to popValues and erase it from hashTable
+        auto range = hashTable.equal_range(hash);
+        auto it = range.first;
+        for (; it != range.second; ++it)
+        {
+            if (it->second == &(*src_it))
+            {
+                break;
+            }
+        }
+        if (it != range.second)
+        {
+            popValues.insert({hash, std::move(*src_it)});
+            hashTable.erase(it);
+        }
+
+        // Try to find the correct element in popValues
+        auto foundIt = popValues.find(*correctHash);
+        if (foundIt != popValues.end())
+        {
+            *src_it = std::move(foundIt->second);
+            popValues.erase(foundIt);
+        }
+        else
+        {
+            // If not found in popValues, get it from hashTable
+            auto correctRange = hashTable.equal_range(*correctHash);
+            if (correctRange.first != correctRange.second)
+            {
+                *src_it = std::move(*(correctRange.first->second));
+                hashTable.erase(correctRange.first);
+            }
+            else
+            {
+                // If we cannot find the correct element in hashTable, we have a problem
+                std::cerr << "Error: could not find correct element in hashTable for hash " << *correctHash << std::endl;
+                std::terminate();
+            }
+        }
     }
-
-    return {hashedValues, hashTable};
-  }
-
-  template< std::forward_iterator InputIterator_t, class HashFunction, class Hash_t, class Value_t >
-  void placeElementsInCorrectPositions(InputIterator_t begin, InputIterator_t end, HashFunction hashFunction, const std::vector<Hash_t> &hashValues, std::unordered_map<Hash_t, Value_t *> &hashTable)
-  {
-    std::unordered_map<Hash_t, Value_t> popValues;
-
-    for (auto src_it = begin, correctHash = hashValues.begin(); src_it != end; src_it++, correctHash++)
-    {
-      auto hash = hashFunction(*src_it);
-      if (hash == *correctHash)
-      {
-        continue;
-      }
-      popValues.insert({hash, std::move(*src_it)});
-      hashTable.erase(hash);
-      auto foundIt = popValues.find(*correctHash);
-      if (foundIt != popValues.end())
-      {
-        *src_it = std::move(foundIt->second);
-        popValues.erase(foundIt);
-      }
-      else
-      {
-        *src_it = std::move(*hashTable[*correctHash]);
-        hashTable.erase(*correctHash);
-      }
-    }
-  }
+}
 
 #endif
